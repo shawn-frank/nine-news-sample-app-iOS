c34c8d5e3ec41d0da46bcb96b0a510cfcbce4450
Shawn Frank <shawn@shawn.design>
2023-03-16 13:57:07 +1000

    minor refactor for smallest thumbnail test

--- Nine News Assessment/Nine News Assessment Unit Tests/NewsServiceTests/NewsAssetManagerTests.swift
+++ Nine News Assessment/Nine News Assessment Unit Tests/NewsServiceTests/NewsAssetManagerTests.swift
@@ -11,25 +11,25 @@ import XCTest
 class NewsAssetManagerUnitTests: XCTestCase {
 
     func test_smallest_thumbnail_size() {
-        let newsImages = [NewsImageModel(id: 1, url: "1000x200.jpg", width: 1000, height: 200),
-                          NewsImageModel(id: 2, url: "300x200.jpg", width: 300, height: 200),
-                          NewsImageModel(id: 3, url: "900x600.jpg", width: 900, height: 600),
-                          NewsImageModel(id: 4, url: "150x200.jpg", width: 150, height: 200)]
+        let newsImages = [NewsImageModel(id: 1, url: "large", width: 1000, height: 200),
+                          NewsImageModel(id: 2, url: "large", width: 300, height: 200),
+                          NewsImageModel(id: 3, url: "large", width: 900, height: 600),
+                          NewsImageModel(id: 4, url: "smallest", width: 150, height: 200)]
         
         let newsAsset = NewsAssetModel(id: 1, headline: "", abstract: "", author: "",
                                        url: "", images: newsImages, timeStamp: 0)
         
-        let newsImages2 = [NewsImageModel(id: 1, url: "1001x200.jpg", width: 1000, height: 200),
-                          NewsImageModel(id: 2, url: "1000x200.jpg", width: 300, height: 200)]
+        let newsImages2 = [NewsImageModel(id: 1, url: "large", width: 1001, height: 200),
+                          NewsImageModel(id: 2, url: "smallest", width: 1000, height: 200)]
         
         let newsAsset2 = NewsAssetModel(id: 1, headline: "", abstract: "", author: "",
                                        url: "", images: newsImages2, timeStamp: 0)
         
         let newsManager = NewsAssetManager()
         
-        XCTAssertEqual("150x200.jpg", newsManager.getThumbnailURL(for: newsAsset),
+        XCTAssertEqual("smallest", newsManager.getThumbnailURL(for: newsAsset),
                        "Smallest image not returned")
-        XCTAssertEqual("1000x200.jpg", newsManager.getThumbnailURL(for: newsAsset2),
+        XCTAssertEqual("smallest", newsManager.getThumbnailURL(for: newsAsset2),
                        "Smallest image not returned")
     }
 
