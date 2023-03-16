54c2022a4a620c70565aaf4d7ae86e8734a530b3
Shawn Frank <shawn@shawn.design>
2023-03-16 14:41:01 +1000

    add UI test module

---
+++ Nine News Assessment/Nine News Assessment UITests/SplashScreenTests.swift
@@ -0,0 +1,15 @@
+//
+//  SplashScreenTests.swift
+//  Nine News Assessment UITests
+//
+//  Created by Shawn Frank on 16/3/2023.
+//
+
+import XCTest
+
+class SplashScreenTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+}
--- Nine News Assessment/Nine News Assessment/System/System Constants/SplashScreenConstants.swift
+++ Nine News Assessment/Nine News Assessment/System/System Constants/SplashScreenConstants.swift
@@ -10,7 +10,7 @@ import Foundation
 extension SystemConstants {
     struct SplashScreen {
         static let splashScreenImageName = "Nine Splash"
-        
+        static let progressViewAccessibilityIdentifier = "com.mindhyve.practice.nine-assessment-splash-screen-activity-indicator"
         static let progressViewWidth: Float = 50
         static let progressViewHeight: Float = 50
         static let progressViewBottomPadding: Float = 50
